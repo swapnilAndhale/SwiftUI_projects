8df455a662cb3cd4f28236496104e128d2679b29
Swapnil <swapnilandhale3@gmail.com>
2022-07-17 17:42:11 +0530

    Countdown first commit

---
+++ CountDown/CountDown/CountDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountDownApp.swift
+//  CountDown
+//
+//  Created by Swapnil on 17/07/22.
+//
+
+import SwiftUI
+
+@main
+struct CountDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
