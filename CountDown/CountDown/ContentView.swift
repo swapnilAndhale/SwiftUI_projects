71a1bfb71d04d297681cba585253e26c602857c3
Swapnil <swapnilandhale3@gmail.com>
2022-07-17 18:30:28 +0530

    Circle created

--- CountDown/CountDown/ContentView.swift
+++ CountDown/CountDown/ContentView.swift
@@ -8,9 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let strokeStyle = StrokeStyle(lineWidth: 15, lineCap: .round)
+    
+    //    let countdownColor: Color = {
+    //        Color,red
+    //    }
+    
+    //let buutonIcon = timerRunning ? "pause.rectangel.fill" : "play.rectangle.fill"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack {
+            Circle()
+                .stroke(Color.red.opacity(0.8), style: strokeStyle)
+                .rotationEffect(.degrees(120))
+            
+            HStack {
+                Label("Pause ", systemImage: "pause")
+                    .foregroundColor(.black)
+                    .font(.title)
+                    .onTapGesture {
+                        //pause function
+                    }
+                
+                Label("Refresh", systemImage: "gobackward")
+                    .foregroundColor(.mint)
+                    .font(.title)
+                
+                Label("Stop", systemImage: "stop")
+                    .foregroundColor(.red)
+                    .font(.title)
+            }
+        }
     }
 }
 
