0be633ee7f6679ae7195973211aec7cf904cc30c
Swapnil <swapnilandhale3@gmail.com>
2022-07-23 17:48:09 +0530

    word check added

--- WordList/WordList/ContentView.swift
+++ WordList/WordList/ContentView.swift
@@ -12,6 +12,10 @@ struct ContentView: View {
     @State private var words = [String]()
     @State private var wordsFromFile = ""
     
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
     var body: some View {
         NavigationView {
             List {
@@ -33,12 +37,32 @@ struct ContentView: View {
             .navigationTitle(wordsFromFile)
             .onSubmit(addNewWord)
             .onAppear(perform: addWordsFromFile)
+            .alert(errorTitle, isPresented: $showingError) {
+                Button("OK", role: .cancel) {}
+            } message: {
+                Text(errorMessage)
+            }
         }
     }
     
     func addNewWord() {
         let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
         guard answer.count > 0 else { return }
+        
+        guard realWord(word: answer) else {
+            showError(title: "Word already used", message: "Use another word")
+            return
+        }
+        guard possibleWord(word: answer) else {
+            showError(title: "Wrong word", message: "Try to put correct word")
+            return
+        }
+        
+        guard isReal(word: answer) else {
+            showError(title: "word not correct", message: "put right word")
+            return
+        }
+        
         withAnimation {
             words.insert(answer, at: 0)
         }
@@ -63,7 +87,35 @@ struct ContentView: View {
         fatalError("allword.rtf not found or load from bundle ")
     }
     
+    func realWord(word: String) -> Bool {
+        !words.contains(word)    // x  != 10   x = [10, 20 ,30 ,40]   !x.contains(10)
+    }
     
+    func possibleWord(word: String) -> Bool {
+        var tempWord = wordsFromFile
+        for letter in word {   // happ y
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location  == NSNotFound
+    }
+    
+    func showError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
+    }
     
 }
 
