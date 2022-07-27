773176b7523519052d889656a93257c0fcaa87bf
Swapnil <swapnilandhale3@gmail.com>
2022-07-27 18:00:05 +0530

    new screen added

---
+++ WFH Expense/WFH Expense/WFH_ExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  WFH_ExpenseApp.swift
+//  WFH Expense
+//
+//  Created by Swapnil on 27/07/22.
+//
+
+import SwiftUI
+
+@main
+struct WFH_ExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
